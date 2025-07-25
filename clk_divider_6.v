58b3fb4a089ba742e6ee7dba0e242185ec58fb9b
Kareem Desouky <95541094+kzsh24@users.noreply.github.com>
2025-07-25 20:07:05 +0300

    Update clk_divider_6.v

--- clk_divider_6.v
+++ clk_divider_6.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/24/2025 11:22:56 AM
-// Design Name: 
-// Module Name: clk_divider_6
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module clk_divider_6(
 input clk ,
 input reset ,
